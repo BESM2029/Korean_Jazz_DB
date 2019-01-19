e874b72a9262c6b65b84c65e49155feae2dd5781
Moon Ho Hwang <moon.hwang@gmail.com>
2019-01-19 10:43:25 -0500

    jason data

---
+++ data/JSON_data.js
@@ -0,0 +1,93 @@
+let JSONdata = [
+{titles:{en:"Nostalgia",kr:"향수"}, 
+    release:"2015.00.00", main_artists:[{en:"Acousticology"}], executive_producer:[{kr:"이재진"}], producers:[{kr:"이재진"},{kr:"조영덕"}, {kr:"김대호"},{kr:"권성민"}],
+    group:[{name:{en:"Acousticology"}, members:[{kr:"조영덕"},{kr:"김대호"}]},{name:"TakeOne Music Studio", members:[{kr:"이재진"},{kr:"최승희"}]},{name:{en:"Sonic Korea"}, members:[{kr:"전훈"}]}],
+    mixing:[{kr:"이재진"}], recording:[{kr:"최승희"}], mastering:[{kr:"전훈"}], photo:[{en:"Alex Ham"}], design:[{kr:"조유림"}], studio_manager:[{kr:"김명기"}],
+    playing_instruments:["guitar","bass"], languages:["en", "kr"],
+disks:[
+    [
+    {track:"01", titles:{en:"Nostalgia",kr:"향수"}, time:"00.04.04", composer:[{kr:"조영덕"}], instruments:{guitar:[{kr:"조영덕"}], bass:[{kr:"김대호"}]}},
+    {track:"02", titles:{en:"Foggy day",kr:"안개"}, time:"00.05.10", composer:[{kr:"조영덕"}], instruments:{guitar:[{kr:"조영덕"}], bass:[{kr:"김대호"}]}},
+    {track:"03", titles:{en:"Blow in the wind",kr:"바람언덕"},  time:"00.06.16", composer:[{kr:"조영덕"}], instruments:{guitar:[{kr:"조영덕"}], bass:[{kr:"김대호"}]}},
+    
+    {track:"04", titles:{en:"Tunnel",kr:"터널"}, time:"00.06.15", composer:[{kr:"조영덕"}],  instruments:{guitar:[{kr:"조영덕"}], bass:[{kr:"김대호"}]}},
+    {track:"05", titles:{en:"Sea Breeze",kr:"바닷 바람"}, time:"00.05.23", composer:[{kr:"조영덕"}],  instruments:{guitar:[{kr:"조영덕"}], bass:[{kr:"김대호"}]}},
+    {track:"06", titles:{en:"Sail Home",kr:"집으로 가는 길"}, time:"00.07.33", composer:[{kr:"김대호"}],  instruments:{guitar:[{kr:"조영덕"}], bass:[{kr:"김대호"}]}},
+    
+    {track:"07", titles:{en:"Gypsyology"}, time:"00.05.22", composer:[{kr:"조영덕"}], instruments:{guitar:[{kr:"조영덕"}], bass:[{kr:"김대호"}]}},
+    {track:"08", titles:{en:"Journey",kr:"여행"}, time:"00.03.45", composer:[{kr:"조영덕"}], instruments:{guitar:[{kr:"조영덕"}], bass:[{kr:"김대호"}]}},
+    {track:"09", titles:{en:"Sausages On the Grill",kr:"그릴 소세지"}, time:"00.05.08", composer:[{kr:"김대호"}], instruments:{guitar:[{kr:"조영덕"}], bass:[{kr:"김대호"}]}}
+    ]
+    
+    ]
+}, // end of album
+
+{titles:{kr:"JAZZ: 째즈로 들어본 우리민요 가요 팝송!"}, 
+    release:"1979.00.00", executive_producer:[{en:"Eom Jin",kr:"엄진"}], producers:[{en:"Lee Pan-geun", kr:"이판근"}],
+    group:[],
+    arrangement:[{en:"Lee Pan-geun", kr:"이판근"}], design:[{en:"Eom Jin", kr:"엄진"}],  
+    playing_instruments:["trumpet", "tenor saxophone", "soprano saxophone", "piano", "bass", "drums", "bongo"], languages:["en", "kr"],
+disks:[
+    [
+    {track:"01", titles:{en:"Arirang(Traditional)",kr:"아리랑(구전민요)"}, time:"00.10.17", 
+      instruments:{trumpet:[{en:"Kang Dae-gwan", kr:"강대관"}], 
+        "tenor saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], "soprano saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], 
+        piano:[{en:"Son Soo-kil", kr:"손수길"}], 
+        bass:[{en:"Lee Soo-young", kr:"이수영"}],
+        drums:[{en:"Choi Se-jin", kr:"쵀세진"}],
+        bongo:[{en:"Choi Se-jin", kr:"쵀세진"}]}},
+    {track:"02", titles:{en:"Bin Ba-Da (Eom Jin) KOMCA",kr:"빈바다"}, time:"00.05.38", composer:[{en:"Eom Jin",kr:"엄진"}],
+    instruments:{trumpet:[{en:"Kang Dae-gwan", kr:"강대관"}], 
+        "tenor saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], "soprano saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], 
+        piano:[{en:"Son Soo-kil", kr:"손수길"}], 
+        bass:[{en:"Lee Soo-young", kr:"이수영"}],
+        drums:[{en:"Choi Se-jin", kr:"쵀세진"}],
+        bongo:[{en:"Choi Se-jin", kr:"쵀세진"}]}},
+    {track:"03", titles:{en:"Bi-Nae-ri-neun Bam-e [Rainy Night in Gerogia] (Tony Joe White) BMI",kr:"비내리는 밤에"}, time:"00.05.00", 
+    composer:[{en:"B.B.KING"}],
+    instruments:{trumpet:[{en:"Kang Dae-gwan", kr:"강대관"}], 
+        "tenor saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], "soprano saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], 
+        piano:[{en:"Son Soo-kil", kr:"손수길"}], 
+        bass:[{en:"Lee Soo-young", kr:"이수영"}],
+        drums:[{en:"Choi Se-jin", kr:"쵀세진"}],
+        bongo:[{en:"Choi Se-jin", kr:"쵀세진"}]}},
+    {track:"04", titles:{en:"Hae Mal-geun A-chim [Softly, as in a Morning Sunrise] (Sigmund Romberg and Oscar Hammerstein II) ASCAP",kr:"해맑은 아침"}, 
+    time:"00.02.50", composer:[{kr:"빅터영"}],
+    instruments:{trumpet:[{en:"Kang Dae-gwan", kr:"강대관"}], 
+        "tenor saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], "soprano saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], 
+        piano:[{en:"Son Soo-kil", kr:"손수길"}], 
+        bass:[{en:"Lee Soo-young", kr:"이수영"}],
+        drums:[{en:"Choi Se-jin", kr:"쵀세진"}],
+        bongo:[{en:"Choi Se-jin", kr:"쵀세진"}]}}
+    ],
+    [
+    {track:"01", titles:{en:"Gasiri (Eom Jin) Copyright control",kr:"가시리"}, time:"00.06.11",  composer:[{en:"Eom Jin",kr:"엄진"}],
+      instruments:{trumpet:[{en:"Kang Dae-gwan", kr:"강대관"}], 
+        "tenor saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], "soprano saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], 
+        piano:[{en:"Son Soo-kil", kr:"손수길"}], 
+        bass:[{en:"Lee Soo-young", kr:"이수영"}],
+        drums:[{en:"Choi Se-jin", kr:"쵀세진"}],
+        bongo:[{en:"Choi Se-jin", kr:"쵀세진"}]}},
+    {track:"02", titles:{en:"Han-o-back-nyun (Traditional)",kr:"한오백년(구전민요)"}, time:"00.06.58", 
+    instruments:{trumpet:[{en:"Kang Dae-gwan", kr:"강대관"}], 
+        "tenor saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], "soprano saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], 
+        piano:[{en:"Son Soo-kil", kr:"손수길"}], 
+        bass:[{en:"Lee Soo-young", kr:"이수영"}],
+        drums:[{en:"Choi Se-jin", kr:"쵀세진"}],
+        bongo:[{en:"Choi Se-jin", kr:"쵀세진"}]}},
+    {track:"03", titles:{en:"Na-ee Mo-den-geot [My Favorite Things] (Richard Rodges and Oscar Hammerstein II) ASCAP",kr:"나의 모든 것"}, 
+    time:"00.08.39",  composer:[{kr:"번슈타인"}],
+    instruments:{trumpet:[{en:"Kang Dae-gwan", kr:"강대관"}], 
+        "tenor saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], "soprano saxophone":[{en:"Kim Su-yeol", kr:"김수열"}], 
+        piano:[{en:"Son Soo-kil", kr:"손수길"}], 
+        bass:[{en:"Lee Soo-young", kr:"이수영"}],
+        drums:[{en:"Choi Se-jin", kr:"쵀세진"}],
+        bongo:[{en:"Choi Se-jin", kr:"쵀세진"}]}} 
+    ]
+    ]
+}// end of album
+];
+
+let jsonStr = JSON.stringify(JSONdata);
+
+console.log(jsonStr);
